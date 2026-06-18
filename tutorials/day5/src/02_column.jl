5f20ccb413c135ab3453600cf9292dbef10dc30d
Jago Strong-Wright <jagoosw@protonmail.com>
2026-06-18 16:52:29 +0200

    fixed up 02

--- tutorials/day5/src/02_column.jl
+++ tutorials/day5/src/02_column.jl
@@ -37,14 +37,14 @@ set!(model, P = 0.1,  Z = 0.1, NO₃ = 10)
 
 Δt = 0.5 * minimum_zspacing(grid) / (200/day) # limited by the fast sinking tracers because of the explicit sinking
 
-simulation = Simulation(model; Δt, stop_time = 365*10days)
+simulation = Simulation(model; Δt, stop_time = 365*3days)
 
 prog(sim) = @info prettytime(sim) * " in " * prettytime(sim.run_wall_time) * " max(P) = $(round(maximum(sim.model.tracers.P), digits = 2))"
 
-add_callback!(simulation, prog, IterationInterval(1000))
+add_callback!(simulation, prog, IterationInterval(10000))
 
 simulation.output_writers[:tracers] = 
-    JLD2Writer(model, model.tracers, 
+    JLD2Writer(model, merge(model.tracers, (; PAR = biogeochemistry.light_attenuation.field)), 
                filename = "02_column.jld2",
                schedule = TimeInterval(1day), 
                overwrite_existing=true)
@@ -56,6 +56,7 @@ run!(simulation)
 # now plot
 
 fds = FieldDataset("02_column.jld2")
+fds["NH₄"] .*= 100 # for visulisation
 
 n = Observable(1)
 z = znodes(fds["P"])
@@ -64,17 +65,31 @@ title = @lift prettytime(fds["P"].times[$n])
 
 fig = Figure(; title)
 
-ax = Axis(fig[1, 1])
+ax0 = Axis(fig[0, 1:3], xlabel = "Surface P (mmolN/m³)", ylabel = "Time (years)")
+ax1 = Axis(fig[1:2, 1], title = "Nutrients")
+ax2 = Axis(fig[1:2, 2], title = "Plankton")
+ax3 = Axis(fig[1:2, 3], title = "Detritus")
 
-lines!(ax, (@lift fds["NO₃"][$n]), z)
-lines!(ax, (@lift fds["NH₄"][$n]), z)
-lines!(ax, (@lift fds["P"][$n]), z)
-lines!(ax, (@lift fds["Z"][$n]), z)
-lines!(ax, (@lift fds["DOM"][$n]), z)
-lines!(ax, (@lift fds["sPOM"][$n]), z)
-lines!(ax, (@lift fds["bPOM"][$n]), z)
+lines!(ax0, fds["P"].times./365days, interior(fds["P"], 1, 1, Nz, :))
+scatter!(ax0, (@lift [fds["P"].times[$n]./365days]), (@lift [interior(fds["P"], 1, 1, Nz, $n)[1, 1, 1]]))
 
-record(fig, "02_column.mp4", 1:Nz, framerate=8) do i
+lines!(ax1, (@lift fds["NO₃"][$n]), z, label = "NO₃")
+lines!(ax1, (@lift fds["NH₄"][$n]), z, label = "100 × NH₄")
+lines!(ax2, (@lift fds["P"][$n]), z, label = "P")
+lines!(ax2, (@lift fds["Z"][$n]), z, label = "Z")
+lines!(ax3, (@lift fds["DOM"][$n]), z, label = "DOM")
+lines!(ax3, (@lift fds["sPOM"][$n]), z, label = "sPOM")
+lines!(ax3, (@lift fds["bPOM"][$n]), z, label = "bPOM")
+
+xlims!(ax1, 0, 40)
+xlims!(ax2, 0, 5)
+xlims!(ax3, 0, 1.5)
+
+axislegend(ax1, position = :rb)
+axislegend(ax2, position = :rb)
+axislegend(ax3, position = :rb)
+
+record(fig, "02_column.mp4", 1:length(fds["P"]), framerate=8) do i
     n[] = i
 end
 
